50e3550867476eaeba0bc9ee358ae11533ba4093
Unknown <josephine.beaune@gmail.com>
2016-11-01 17:50:27 +0100

    Clean first Server

--- server/server.py
+++ server/server.py
@@ -1,54 +1,43 @@
-
 # coding=utf-8
-from flask import Flask
-app = Flask(__name__)
-
-
-from jinja2 import Environment, PackageLoader
-jinja_env = Environment(loader=PackageLoader('server','Views'))
-
 
 import requests
 
-data = requests.get('https://api.smartcitizen.me/v0/devices/3740')
-dataJ = data.json()
-#
-# for sensor in dataJ['data']['sensor']:
-#     if sensor ['id'] ==4:
-#         temperature = sensor['value']
+from flask import Flask
+app = Flask(__name__)
 
-# rep = requests.post('http://imagebin.ca/upload.php',
-#                     files={
-#                     'file':open('/Users/corinofontana/Desktop/Test.png', 'rb')
-#                     }
-#                     )
-# print(rep)
-# print(rep.text)
-#
-# lien = rep.text;
-#
-# x=lien.split('url:')#split URL
-# a,b=x
-# print(b)#b=url image
-
-# @app.route("/")# text ecran
-# def Picture():
-#     return b
-
+from jinja2 import Environment, PackageLoader
+jinja_env = Environment(loader=PackageLoader('serveur','view'))
 
 @app.route("/")
-def hello():
-
-    data = requests.get("https://api.smartcitizen.me/v0/devices/3740")
+def function():
+    data = requests.get('https://api.smartcitizen.me/v0/devices/3738')
     dataJ = data.json()
-    #print(dataJ['data']['sensors'][3]['value'],dataJ['data']['sensors'][3]['unit'])
-    tempj = dataJ['data']['sensors'][3]['value']
-    tempj=str(tempj)
 
+    gasV = 0
+    gasU = '?'
 
-    base = jinja_env.get_template('Base.html')
-    return base.render(temp_value=tempj)#inserer image=b pour voir l'image upload
+    for sensor in dataJ['data']['sensors']:
+        if sensor['description']=="Temperature":
+            temp = sensor['value']
+            temp=round(temp,2)
+            tempU = sensor['unit']
+
+    for sensor in dataJ['data']['sensors']:
+        if sensor['description']=="Humidity":
+            HumV = sensor['value']
+            HumV=round(HumV,2)
+            HumU = sensor['unit']
+
+    for sensor in dataJ['data']['sensors']:
+        if sensor['description']=="NO2":
+            gasV = sensor['value']
+            gasV=round(gasV,2)
+            gasU = sensor['unit']
+
+    base = jinja_env.get_template('base.html')
+    return base.render(temp_value=temp,temp_unit=tempU,hum_value=HumV,hum_unit=HumU,gas_value=gasV,gas_unit=gasU)
+
 
 
 if __name__ == "__main__":
-    app.run(port=1333)
+    app.run(port=10000)
