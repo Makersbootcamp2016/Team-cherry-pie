5c91e2f2d6a1616281bf48a710c80b757daad03d
Ines Joly <ines.joly@yahoo.fr>
2016-11-04 11:33:18 +0100

    take shot

--- server/server.py
+++ server/server.py
@@ -4,7 +4,6 @@
 import os, glob
 import requests
 import json
-import dateutil.parser
 import datetime, time
 from shutil import copyfile
 from flask import Flask, request, redirect, url_for
@@ -45,7 +44,28 @@ def function():
     base = jinja_env.get_template('base.html')
     return base.render(temp_value=temp,temp_unit=tempU,hum_value=HumV,hum_unit=HumU,gas_value=gasV,gas_unit=gasU)
 
+@app.route("/clak")
+def doesrasptakeapic():
+    time.sleep(30)
+    return "YES"
 
+@app.route("/shot", methods=['POST'])
+def shot():
+   if request.method == 'POST':
+       # check if the post request has the file part
+       if 'image' not in request.files:
+           return 'ERROR: No file..'
+
+       file = request.files['image']
+       if not file or file.filename == '':
+           return 'ERROR: Wrong file..'
+
+       # Save Snapshot with Timestamp
+       filepath = os.path.join(os.path.dirname(os.path.abspath(__file__))+'/static/upload/', "usershot.jpg")
+       file.save(filepath)
+
+       return 'SUCCESS'
+   return 'ERROR: You\'re lost Dave..'
 
 
 
